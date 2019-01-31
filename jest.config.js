9ba58b9a0b84554c53beb59c413015bab6059481
Richard <2219269374@qq.com>
2019-01-31 19:07:47 +0800

    create react app.

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  testURL: 'http://localhost',
+  clearMocks: true,               // 每次测试之间自动清除模拟调用和实例
+  coverageDirectory: "coverage",  // 输出覆盖率
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.test.js'],
+};
