f6c21d0b5dab701e96c334752d267a0d08a3bec5
Richard <2219269374@qq.com>
2019-02-01 14:19:29 +0800

    create project.

--- setupTestFramework.js
+++
@@ -1,8 +0,0 @@
-import "whatwg-fetch";
-import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
-Enzyme.configure({ adapter: new Adapter() });
-
-global.APP_ID = "";
-global.VERSION_STRING = "TESTING";
